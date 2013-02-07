f35966f5c4a81797203d2f6429769e380bb19d80
Dzidas <kafka@oras.local>
2013-02-07 18:23:02 +0100

    .72267 user - friends coordinates have been added

--- user_friends_coord.r
+++ user_friends_coord.r
@@ -2,11 +2,12 @@
 #tmp=merge(user,usr_coor,x.by=6,y.by=1)
 
 
-user_friends_coord=apply(tail(user[which(is.na(user$user_lat)),],200),1,function(x){
+user_friends_coord=apply((user[which(is.na(user$user_lat)),]),1,function(x){
   #get user's friends
   x[1]=as.numeric(x[1])
-  #if(!any(friends$user==x[1]))#no friends
-  #  return(data.frame(lat=NA,long=NA))
+  #print(x[1])
+  if(!any(friends$user==x[1]))#no friends
+    return(data.frame(lat=NA,long=NA))
   fnd=strsplit(as.character(friends[which(friends$user==x[1]),]$friends),' ')[[1]]
     
   rez=user[which(user$user_id%in%fnd),]
@@ -19,10 +20,12 @@ user_friends_coord=apply(tail(user[which(is.na(user$user_lat)),],200),1,function
   if(length(lat)>3 & !(length(unique(lat))==1 & length(unique(long))==1)){
     tryCatch({
       set.seed(33344)
-      centers=ifelse(unique(lat)*2<length(lat),unique(lat),2)
-    kmean=kmeans(data.frame(lat,long),centers)
+      if(length(unique(lat))>2 | length(unique(long))>2&length(lat)>9)
+        kmean=kmeans(data.frame(lat,long),3)
+      else
+        kmean=kmeans(data.frame(lat,long),2)
     k_max=aggregate(kmean$cluster,list(kmean$cluster),length)
-    rez=c(as.numeric(t(kmean$centers[which(max(k_max[,2])==k_max[,2]),])),paste(lat))
+    rez=c(as.numeric(t(kmean$centers[which(max(k_max[,2])==k_max[,2]),])))#,paste(lat))
     },
       warning=function(w){print('warn')},
       error=function(e){return(data.frame(lat=lat[1],long=long[1]))
